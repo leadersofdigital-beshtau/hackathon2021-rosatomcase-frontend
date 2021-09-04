f02eb191671a6e7442e56d1c8c7036180bd096c4
Artem Sokolov <artemsokolov@hotmail.com>
2021-09-04 03:50:00 +0300

    Доработан компонент сайдбара

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,15 @@
 <template>
   <div class="app">
-    <div class="app__header">
-      <Header></Header>
-    </div>
     <main class="app__main">
       <router-view/>
     </main>
-    <div class="app__footer">
-      <Footer></Footer>
-    </div>
   </div>
 </template>
 
 <script>
-import Header from "@/components/Header";
-import Footer from "@/components/Footer";
-
 export default {
   name: 'Home',
-  components: {
-    Footer,
-    Header
-  }
+  components: {}
 }
 </script>
 
